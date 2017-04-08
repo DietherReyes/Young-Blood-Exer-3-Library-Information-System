ad252558d8aca968687dbd09e0e7626e4d614941
DietherReyes <cbreyes4@up.edu.ph>
2017-04-09 07:37:20 +0800

    Add files via upload

---
+++ Encyclopedia.java
@@ -0,0 +1,6 @@
+package books;
+public class Encyclopedia extends Book{
+  public Encyclopedia(String id, String title, String author, String year){
+    super(id, title, author, year);
+  }
+}
---
+++ Novels.java
@@ -0,0 +1,6 @@
+package books;
+public class Novels extends Book{
+  public Novels(String id, String title, String author, String year){
+    super(id, title, author, year);
+  }
+}
