b9175d42dbf6f151574b81663c8d17b6be096a88
DietherReyes <cbreyes4@up.edu.ph>
2017-04-09 07:02:31 +0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,18 +1,29 @@
 package Books;
 
 public class Book{
-  private int id;
+  private String id; //converter in main 
   private String title;
   private String author;
   private String year;
 
-  public Book(int id, String title, String Author, String year){
+  public Book(String id, String title, String Author, String year){
     this.title = title;
     this.author = author;
     this.year = year;
-    
+    this.id = id; 
   }
-
-
+  
+  String getID(){
+    return this.id;
+  }
+  String getTitle(){
+    return this.id;
+  }
+  String getAuthor(){
+    return this.author;
+  }
+  String getYear(){
+    return this.year
+  }
 }
   
