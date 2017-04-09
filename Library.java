6d27171f27866ad4d4429407b2493f589183f9cd
michael3096 <iaurriza@up.edu.ph>
2017-04-09 15:54:23 +0800

    added comments

--- Library.java
+++ Library.java
@@ -9,22 +9,22 @@ public class Library{
     library = new HashMap<String,ArrayList<Book>>();
   }
   public void addBook(Book book){
-    if(library.containsKey(book.getTitle())){
-        bookinventory = library.get(book.getTitle());
-        bookinventory.add(book);
-      }else{
-        bookinventory = new ArrayList<Book>();
-        bookinventory.add(book);
-        library.put(book.getTitle(),bookinventory);
+    if(library.containsKey(book.getTitle())){       //if book title is in the key list of hashmap
+        bookinventory = library.get(book.getTitle());         //gets the array
+        bookinventory.add(book);                              //inputs book into the array
+      }else{  
+        bookinventory = new ArrayList<Book>();        //else creates new array
+        bookinventory.add(book);                      //adds book to array
+        library.put(book.getTitle(),bookinventory);   //adds newkey to hashmap
       }
   }
   public void viewBooksInLib(){
     ArrayList<Book> arr;
 
-    if(!library.isEmpty()){
-      for(String book : library.keySet()){
-        arr = library.get(book);
-        arr.get(0).displayInfo();    
+    if(!library.isEmpty()){                       // if hashmap is not empty
+      for(String book : library.keySet()){            //loop for all hashmap values
+        arr = library.get(book);                      //gets array of every hashmap value
+        arr.get(0).displayInfo();                     //displays info of the book inside the array
       }
       System.out.println("HERE");
     }else{
