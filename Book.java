cd68ef0e127fe2140db1a5be920eb8f19218bd51
DietherReyes <cbreyes4@up.edu.ph>
2017-04-09 14:23:32 +0800

    Edits

--- Book.java
+++ Book.java
@@ -1,16 +1,23 @@
 package books;
 
 public class Book{
-  private String id; //converter in main
-  private String title;
-  private String author;
-  private String year;
+  protected String id; //converter in main
+  protected String title;
+  protected String author;
+  protected String year;
+
 
   public Book(String id, String title, String author, String year){
     this.title = title;
     this.author = author;
     this.year = year;
     this.id = id;
+  }
+  public void displayInfo(){
+    System.out.println("Book ID: " + this.id);
+    System.out.println("Book Title: " + this.title);
+    System.out.println("Book Author: " + this.id);
+    System.out.println("Year Published: " + this.id);
   }
                 //public modifier
   public String getID(){
--- Main.java
+++ Main.java
@@ -16,19 +16,28 @@ public class Main{
     int choice;
     do{
       System.out.println("==============MAIN MENU==============");
-      System.out.println("[1] Borrow Book");
-      System.out.println("[2] Return Book");
-      System.out.println("[3] View Books In Library");
-      System.out.println("[4] View Books In User ");
+      System.out.println("[1] Add Book");
++     System.out.println("[2] Borrow Book");
++     System.out.println("[3] Return Book");
++     System.out.println("[4] View Books In Library");
++     System.out.println("[5] View Books In User ");
       System.out.println("[0] Exit ");
       System.out.println("=====================================");
       choice = intInput("Choice");
       switch(choice){
-        case 1: break;
-        case 2: break;
-        case 3: break;
-        case 4: break;
-        default: System.out.println("Invalid Choice!!!"); break;
+        case 1:
+  +            //library.addBook(Integer.toHexString(bookCounter++), strInput("Book Title"), strInput("Book Author"), strInput("Year Published"));
+  +            break;
+       case 2: title = strInput("Title of the Book to be Borrowed");
+               break;
+  +
+       case 3: title = strInput("Title of the Book to be Returned")
+               break;
+  +
+  +    case 4: library.viewBooksInLib();
+  +            break;
+  +    case 5: library.viewBooksInUser();
+  +            break;
       }
     }while(choice != 0);
   }
