ad7bfd29131d72e19fdf5363d385633e225b539c
DietherReyes <cbreyes4@up.edu.ph>
2017-04-09 14:29:06 +0800

    import java.lang.*

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.*;
 public class Main{
   private static int intInput(String str){
     Scanner intScan = new Scanner(System.in); //method to input integers
@@ -17,27 +18,27 @@ public class Main{
     do{
       System.out.println("==============MAIN MENU==============");
       System.out.println("[1] Add Book");
-+     System.out.println("[2] Borrow Book");
-+     System.out.println("[3] Return Book");
-+     System.out.println("[4] View Books In Library");
-+     System.out.println("[5] View Books In User ");
+      System.out.println("[2] Borrow Book");
+      System.out.println("[3] Return Book");
+      System.out.println("[4] View Books In Library");
+      System.out.println("[5] View Books In User ");
       System.out.println("[0] Exit ");
       System.out.println("=====================================");
       choice = intInput("Choice");
       switch(choice){
         case 1:
-  +            //library.addBook(Integer.toHexString(bookCounter++), strInput("Book Title"), strInput("Book Author"), strInput("Year Published"));
-  +            break;
+              //library.addBook(Integer.toHexString(bookCounter++), strInput("Book Title"), strInput("Book Author"), strInput("Year Published"));
+              break;
        case 2: title = strInput("Title of the Book to be Borrowed");
                break;
-  +
+
        case 3: title = strInput("Title of the Book to be Returned")
                break;
-  +
-  +    case 4: library.viewBooksInLib();
-  +            break;
-  +    case 5: library.viewBooksInUser();
-  +            break;
+
+      case 4: library.viewBooksInLib();
+              break;
+      case 5: library.viewBooksInUser();
+              break;
       }
     }while(choice != 0);
   }
