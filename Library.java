7166762ba7031a5300dc13c02d3bd25aadb1c92f
DietherReyes <cbreyes4@up.edu.ph>
2017-04-09 19:55:19 +0800

    Update Library.java

--- Library.java
+++ Library.java
@@ -2,11 +2,11 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.ArrayList;
 import java.io.FileWriter;
-import books.Book;
+import java.io.FileReader;
+
 public class Library{
   private HashMap<String,ArrayList<Book>> library;
   private ArrayList<Book> bookinventory;
-  private Book tempbook;
   public Library(){
     library = new HashMap<String,ArrayList<Book>>();
   }
@@ -21,31 +21,20 @@ public class Library{
       }
   }
   public void viewBooksInLib(){
+    ArrayList<Book> arr;
 
     if(!library.isEmpty()){                       // if hashmap is not empty
       for(String book : library.keySet()){            //loop for all hashmap values
-        bookinventory = library.get(book);                      //gets array of every hashmap value
-        for(Book temp : bookinventory){
-          temp.displayInfo();
+        arr = library.get(book);                      //gets array of every hashmap value
+        for(Book temp : arr){
+          temp.displayInfo();                      //displays info of the book inside the array
+          System.out.println("");
         }
+
       }
     }else{
       System.out.println("Library is Empty");
-    }
-  }
-   public Book removeBook(String title){
-    if(library.containsKey(title)){             //if book is in hashmap
-      bookinventory = library.get(title);       //get the array
-      tempbook = bookinventory.get(0);          //get the book in the array
-      bookinventory.remove(0);                  //remove a book in tje array
-      if(bookinventory.size()==0){              //if array is empty delete hashmap
-        library.remove(title);
-      }
-      System.out.println(tempbook.getTitle()+" has been borrowed");
-      return tempbook;
-    }else{
-      System.out.println("The book titled "+title+" has not been found");
-      return null;
+
     }
   }
   public void saveFileLibrary(){
@@ -58,7 +47,7 @@ public class Library{
 
                 for(Book temp : arr){
                   //writes details(CSV);
-                   fw.write(temp.getID() + "," + temp.getTitle() + "," + temp.getAuthor() + "," + temp.getYear()  + "\r\n" );
+                   fw.write(temp.getID() + "," + temp.getType() + "," + temp.getTitle() + "," + temp.getAuthor() + "," + temp.getYear() + "\r\n" );
                 }
 
               }
@@ -67,6 +56,11 @@ public class Library{
               System.out.println("Library is Empty");
             }
     }catch(Exception e){System.out.println(e);}
+  }
 
+  public void loadFileLibrary(){
+    //readline
+    //array = split
+    //add
   }
 }
