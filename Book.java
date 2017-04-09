8923596d5272ef5aad35a9adb7d085b6ae55106f
DietherReyes <cbreyes4@up.edu.ph>
2017-04-09 13:08:56 +0800

    Edits in Main.java and Book.java
    Book.java:
    added a method viewInfo
    Main.java:
    updated main

--- Book.java
+++ Book.java
@@ -1,10 +1,10 @@
 package books;
 
 public class Book{
-  private String id; //converter in main
-  private String title;
-  private String author;
-  private String year;
+  protected String id; //converter in main
+  protected String title;
+  protected String author;
+  protected String year;
 
   public Book(String id, String title, String author, String year){
     this.title = title;
@@ -12,7 +12,12 @@ public class Book{
     this.year = year;
     this.id = id;
   }
-
+  public void displayInfo(){
+    System.out.println("Book ID: " + this.id);
+    System.out.println("Book Title: " + this.title);
+    System.out.println("Book Author: " + this.id);
+    System.out.println("Year Published: " + this.id);
+  }
   String getID(){
     return this.id;
   }
--- Main.java
+++ Main.java
@@ -1,33 +1,44 @@
 import java.util.Scanner;
+import java.lang.*;
 public class Main{
   private static int intInput(String str){
-    Scanner intScan = new Scanner(System.in); //method to input integers
+    Scanner intScan = new Scanner(System.in); //method to get integer input
     System.out.print("Enter " + str + ": ");
     int intinput = intScan.nextInt();
     return intinput;
   }
-  private static String strInput(String str){// method to input string
+  private static String strInput(String str){// method to get string input
     Scanner strScan = new Scanner(System.in);
     System.out.print("Enter " + str + ": ");
     String strinput = intScan.nextLine();
     return strinput;
   }
   public static void main(String[] args){
-    int choice;
+    int choice,bookCounter = 0;//counts the instance of books
+    String title;
+    Library libary = new Library();
     do{
       System.out.println("==============MAIN MENU==============");
-      System.out.println("[1] Borrow Book");
-      System.out.println("[2] Return Book");
-      System.out.println("[3] View Books In Library");
-      System.out.println("[4] View Books In User ");
+      System.out.println("[1] Add Book");
+      System.out.println("[2] Borrow Book");
+      System.out.println("[3] Return Book");
+      System.out.println("[4] View Books In Library");
+      System.out.println("[5] View Books In User ");
       System.out.println("[0] Exit ");
       System.out.println("=====================================");
       choice = intInput("Choice");
       switch(choice){
-        case 1: break;
+        case 1:
+                //library.addBook(Integer.toHexString(bookCounter++), strInput("Book Title"), strInput("Book Author"), strInput("Year Published"));
+                break;
         case 2: break;
+                title = strInput("Title of the Book to be Borrowed");
         case 3: break;
-        case 4: break;
+                title = strInput("Title of the Book to be Returned");
+        case 4: library.viewBooksInLib();
+                break;
+        case 5: library.viewBooksInUser();
+                break;
         default: System.out.println("Invalid Choice!!!"); break;
       }
     }while(choice != 0);
