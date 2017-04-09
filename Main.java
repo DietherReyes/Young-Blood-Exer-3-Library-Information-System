05881971531c561b9f622154b7282e51516f28b3
michael3096 <iaurriza@up.edu.ph>
2017-04-09 15:36:20 +0800

    removed //addBook comment

--- Main.java
+++ Main.java
@@ -27,7 +27,7 @@ public class Main{
       choice = intInput("Choice");
       switch(choice){
         case 1:
-              //library.addBook(new Book(Integer.toHexString(bookCounter++), strInput("Book Title"), strInput("Book Author"), strInput("Year Published")));
+              library.addBook(new Book(Integer.toHexString(bookCounter++), strInput("Book Title"), strInput("Book Author"), strInput("Year Published")));
               break;
        case 2: title = strInput("Title of the Book to be Borrowed");
                break;
